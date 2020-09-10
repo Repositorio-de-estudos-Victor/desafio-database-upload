0761f6e6da7ba4daf359c7b019b2943fa4798dd6
Victor Rodrigues Novais <victor96novais@gmail.com>
2020-09-10 02:46:27 -0300

    Criacao do banco e migrations, definicao da model

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,11 +1,50 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('category')
 class Category {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
 export default Category;
+
+// import User from './User';
+
+// @Entity('appointments')
+// class Appointment {
+//   @PrimaryGeneratedColumn('uuid')
+//   id: string;
+
+//   @Column()
+//   provider_id: string;
+
+//   @ManyToOne(() => User)
+//   @JoinColumn({ name: 'provider_id' })
+//   provider: User;
+
+//   @Column('timestamp with time zone')
+//   date: Date;
+
+//   @CreateDateColumn()
+//   created_at: Date;
+
+//   @UpdateDateColumn()
+//   updated_at: Date;
+// }
+
+// export default Appointment;
